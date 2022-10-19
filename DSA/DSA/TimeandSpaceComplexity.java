939e69d9719f7b643397fd3cc86e7b1123e5ba90
im-kamil <kamilnasar786@gmail.com>
2022-10-19 12:35:22 +0530

    DSA short notes

---
+++ DSA/DSA/TimeandSpaceComplexity.java
@@ -0,0 +1,5 @@
+// Time Complexity 
+/* The time complexity of an algorithm quantifies the amount of time taken by an algorithm to run as a function of the length of the input. Note that the time to run is a function of the length of the input and not the actual execution time of the machine on which the algorithm is running on.*/
+
+// Space Complexity 
+/* The space complexity of an algorithm quantifies the amount of space taken by an algorithm to run as a function of the length of the input. Consider an example: Suppose a problem to find the frequency of array elements. */
